0a8d4db8abe778332a55d202729d4a03a3b7ac7b
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-10-05 16:35:05 +0200

    var -> val.

--- test/PolynomialArithmetic.kt
+++ test/PolynomialArithmetic.kt
@@ -1,6 +1,7 @@
+
 import group14.integer.ModularInteger
 import group14.polynomial.Polynomial
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class PolynomialArithmetic {
@@ -8,78 +9,78 @@ class PolynomialArithmetic {
     @Test
     fun `AddTest`() {
         println("Add")
-        var p1 = Polynomial(5, 4, 3, 1, 2, 3)
-        var p2 = Polynomial(5, 1, 2, 2, 1, 2, 1, 2, 1)
+        val p1 = Polynomial(5, 4, 3, 1, 2, 3)
+        val p2 = Polynomial(5, 1, 2, 2, 1, 2, 1, 2, 1)
 
-        var expected = Polynomial(5, 0, 0, 3, 3, 0, 1, 2, 1)
-        var result = p1 + p2
+        val expected = Polynomial(5, 0, 0, 3, 3, 0, 1, 2, 1)
+        val result = p1 + p2
 
         assertEquals(expected.toString(), result.toString())
 
-        var m1 = ModularInteger(4, 5);
-        var expected2 = Polynomial(5, 3, 3, 1, 2, 3)
-        var result2 = p1 + m1
+        val m1 = ModularInteger(4, 5);
+        val expected2 = Polynomial(5, 3, 3, 1, 2, 3)
+        val result2 = p1 + m1
         assertEquals(expected2.toString(), result2.toString())
 
-        var expected3 = p1
-        var result3 = p1 + Polynomial(emptyArray(), 5)
+        val expected3 = p1
+        val result3 = p1 + Polynomial(emptyArray(), 5)
         assertEquals("Empty add", expected3.toString(), result3.toString())
 
-        var expected4 = p1
-        var result4 = Polynomial.zero(5) + p1
+        val expected4 = p1
+        val result4 = Polynomial.zero(5) + p1
         assertEquals("Empty first parameter", expected4.toString(), result4.toString())
     }
 
     @Test
     fun `MinusTest`() {
         println("Minus")
-        var p1 = Polynomial(5, 4, 3, 1, 2, 3)
-        var p2 = Polynomial(5, 1, 2, 2, 1, 2, 1, 2, 1)
+        val p1 = Polynomial(5, 4, 3, 1, 2, 3)
+        val p2 = Polynomial(5, 1, 2, 2, 1, 2, 1, 2, 1)
 
-        var expected = Polynomial(5, 3, 1, 4, 1, 1, 4, 3, 4)
-        var result = p1 - p2
+        val expected = Polynomial(5, 3, 1, 4, 1, 1, 4, 3, 4)
+        val result = p1 - p2
         assertEquals(expected.toString(), result.toString())
 
-        var m1 = ModularInteger(4, 5);
-        var expected2 = Polynomial(5, 0, 3, 1, 2, 3)
-        var result2 = p1 - m1
+        val m1 = ModularInteger(4, 5);
+        val expected2 = Polynomial(5, 0, 3, 1, 2, 3)
+        val result2 = p1 - m1
         assertEquals(expected2.toString(), result2.toString())
 
-        var expected3 = Polynomial(emptyArray(), 5)
-        var result3 = p1 - p1
+        val expected3 = Polynomial(emptyArray(), 5)
+        val result3 = p1 - p1
         assertEquals("Zero as result", expected3.toString(), result3.toString())
 
-        var expected4 = p1
-        var result4 = p1 - Polynomial(emptyArray(), 5)
+        val expected4 = p1
+        val result4 = p1 - Polynomial(emptyArray(), 5)
         assertEquals("Empty subtract", expected4.toString(), result4.toString())
 
-        var expected5 = Polynomial(5, 1, 2, 4, 3, 2)
-        var result5 = Polynomial(emptyArray(), 5) - p1
+        val expected5 = Polynomial(5, 1, 2, 4, 3, 2)
+        val result5 = Polynomial(emptyArray(), 5) - p1
         assertEquals("Empty first parameter", expected5.toString(), result5.toString())
     }
 
     @Test
     fun `MultTest`() {
         println("Multiply")
-        var p1 = Polynomial(5, 4, 3, 1, 2, 3)
-        var p2 = Polynomial(5, 1, 2, 2, 1, 2, 1, 2, 1)
+        val p1 = Polynomial(5, 4, 3, 1, 2, 3)
+        val p2 = Polynomial(5, 1, 2, 2, 1, 2, 1, 2, 1)
 
-        var expected = Polynomial(5, 4, 1, 0, 4, 0, 1, 1, 3, 3, 3, 3, 3)
-        var result = p1 * p2
+        val expected = Polynomial(5, 4, 1, 0, 4, 0, 1, 1, 3, 3, 3, 3, 3)
+        val result = p1 * p2
 
         assertEquals(expected.toString(), result.toString())
 
-        var m1 = ModularInteger(4, 5);
-        var expected2 = Polynomial(5, 1, 2, 4, 3, 2)
-        var result2 = p1 * m1
+        val m1 = ModularInteger(4, 5);
+        val expected2 = Polynomial(5, 1, 2, 4, 3, 2)
+        val result2 = p1 * m1
         assertEquals(expected2.toString(), result2.toString())
 
-        var expected3 = Polynomial(emptyArray(), 5)
-        var result3 = p1 * Polynomial(emptyArray(), 5)
+        val expected3 = Polynomial(emptyArray(), 5)
+        val result3 = p1 * Polynomial(emptyArray(), 5)
         assertEquals("Empty multiply", expected3.toString(), result3.toString())
 
-        var expected4 = Polynomial.zero(5)
-        var result4 = Polynomial(emptyArray(), 5) * p1
+        val expected4 = Polynomial.zero(5)
+        val result4 = Polynomial(emptyArray(), 5) * p1
         assertEquals("Empty first parameter", expected4.toString(), result4.toString())
     }
 
