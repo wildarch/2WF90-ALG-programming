7241217c5bfa3a0e5320d135da48859483109a1d
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-10-10 15:41:48 +0200

    Added test suite.

---
+++ test/group14/AlgebraTestSuite.kt
@@ -0,0 +1,21 @@
+package group14
+
+import group14.field.FiniteFieldTest
+import group14.integer.ModularIntegerTest
+import group14.polynomial.PolynomialArithmetic
+import group14.polynomial.PolynomialTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+/**
+ * @author Ruben Schellekens
+ */
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+        UtilKtTest::class,
+        FiniteFieldTest::class,
+        ModularIntegerTest::class,
+        PolynomialArithmetic::class,
+        PolynomialTest::class
+)
+object AlgebraTestSuite
