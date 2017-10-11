af763cf51ba92f3f496e9f24bd8dc52520ad9c94
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-10-11 13:08:24 +0200

    Cleaned up branch.

--- src/group14/Algebra.kt
+++ src/group14/Algebra.kt
@@ -1,24 +1,6 @@
 package group14
 
-import group14.parser.Lexer
-import group14.parser.ParseException
-import group14.parser.Parser
-
 fun main(args: Array<String>) {
-//    val input = "[6*X^8+19X^4-4X^3--4X^2+X+-13] + [x+1] (mod 11)"
-//    val input = "3 + [5X^12 + 5X^3] ^ 7"
-    val input = "[3, 6, 7] * -5 (mod 181) = [3+16*X-7X^2] (mod [181+3X^-16*X^9])"
-    println(">>> $input")
-
-    try {
-        val lexer = Lexer(input)
-        val parser = Parser(lexer)
-        println("\n${parser.constructParseTree()}")
-    }
-    catch (e: ParseException) {
-        System.err.println(" ".repeat(4 + e.column) + "^")
-        e.printStackTrace()
-    }
 }
 
 fun Any?.println() = println(this)
--- test/group14/AlgebraTestSuite.kt
+++ test/group14/AlgebraTestSuite.kt
@@ -3,6 +3,7 @@ package group14
 import group14.field.FiniteFieldTest
 import group14.integer.ModularIntegerTest
 import group14.parser.LexerTest
+import group14.parser.ParserTest
 import group14.polynomial.PolynomialArithmetic
 import group14.polynomial.PolynomialTest
 import org.junit.runner.RunWith
@@ -18,6 +19,7 @@ import org.junit.runners.Suite
         ModularIntegerTest::class,
         PolynomialArithmetic::class,
         PolynomialTest::class,
-        LexerTest::class
+        LexerTest::class,
+        ParserTest::class
 )
 object AlgebraTestSuite
