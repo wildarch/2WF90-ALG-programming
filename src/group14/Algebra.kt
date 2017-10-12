161b2bc202a2fc8365dacb2da836a804185dc7c2
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-10-12 17:36:48 +0200

    Basic in main.

--- src/group14/Algebra.kt
+++ src/group14/Algebra.kt
@@ -1,6 +1,25 @@
 package group14
 
+import group14.parser.Lexer
+import group14.parser.ParseException
+import group14.parser.Parser
+import group14.parser.TreePrinter
+
 fun main(args: Array<String>) {
+    print(">>> ")
+    val input = readLine() ?: ""
+
+    try {
+        val lexer = Lexer(input)
+        val parser = Parser(lexer)
+        val tree = parser.constructParseTree()
+        println()
+        TreePrinter(tree).print().println()
+    }
+    catch (e: ParseException) {
+        System.err.println("    ${" ".repeat(e.column)}^")
+        System.err.println("ParseException: ${e.message}")
+    }
 }
 
 fun Any?.println() = println(this)
