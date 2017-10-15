f5d9d45fb728d0dca1443046d5c1c5c42e0c52a4
codemaster97 <erik_97@live.nl>
2017-10-15 11:23:42 +0200

    PolynomialEuclid's Test

--- test/group14/polynomial/PolynomialEuclidsTest.java
+++ test/group14/polynomial/PolynomialEuclidsTest.java
@@ -71,14 +71,23 @@ public class PolynomialEuclidsTest {
     @Test
     public void PolynomialTest() {
         PolynomialEuclids euclid;
-        Polynomial a = new Polynomial(5, 1, 0, 4);
-        Polynomial b = new Polynomial(5, 1, 2, 1);
+        Polynomial a = new Polynomial(2, 1, 1, 0, 1);
+        Polynomial b = new Polynomial(2, 0, 1);
         euclid = new PolynomialEuclids(a, b);
         euclid.execute();
         Polynomial x, y, gcd;
         x = euclid.getX();
         y = euclid.getY();
         gcd = euclid.getGcd();
+
+        //System.out.println("A: " + a.toPolynomialString());
+        //System.out.println("B: " + b.toPolynomialString());
+        //System.out.println("GCD: " + gcd.toPolynomialString());
+        //System.out.println("X: " + x.toPolynomialString());
+        //System.out.println("Y: " + y.toPolynomialString());
+        assertEquals("GCD should equal 1", new Polynomial(2, 1), gcd);
+        assertEquals("X should equal 1", new Polynomial(2, 1, 0, 0).toPolynomialString(), x.toPolynomialString());
+        assertEquals("Y should equal X^2 + 1", new Polynomial(2, 1, 0, 1), y);
     }
 
 }
