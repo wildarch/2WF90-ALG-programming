8686eb01bed83d6fb0fa3eee4d9d19a40e6f395a
wildarch <daandegraaf9@gmail.com>
2017-10-17 11:36:13 +0200

    Update tests

--- test/group14/field/AdditionTableTest.kt
+++ test/group14/field/AdditionTableTest.kt
@@ -16,12 +16,12 @@ class AdditionTableTest {
         val table = AdditionTable(field)
 
         val expected =
-                """             |  0 (ℤ/2ℤ)       1 (ℤ/2ℤ)       X (ℤ/2ℤ)       X + 1 (ℤ/2ℤ) """ + System.lineSeparator() +
-                        """––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––""" + System.lineSeparator() +
-                        """0 (ℤ/2ℤ)     |  0 (ℤ/2ℤ)       1 (ℤ/2ℤ)       X (ℤ/2ℤ)       X + 1 (ℤ/2ℤ) """ + System.lineSeparator() +
-                        """1 (ℤ/2ℤ)     |  1 (ℤ/2ℤ)       0 (ℤ/2ℤ)       X + 1 (ℤ/2ℤ)   X (ℤ/2ℤ)     """ + System.lineSeparator() +
-                        """X (ℤ/2ℤ)     |  X (ℤ/2ℤ)       X + 1 (ℤ/2ℤ)   0 (ℤ/2ℤ)       1 (ℤ/2ℤ)     """ + System.lineSeparator() +
-                        """X + 1 (ℤ/2ℤ) |  X + 1 (ℤ/2ℤ)   X (ℤ/2ℤ)       1 (ℤ/2ℤ)       0 (ℤ/2ℤ)     """
+                """             |  0 (ℤ/2ℤ)      1 (ℤ/2ℤ)      X (ℤ/2ℤ)      X + 1 (ℤ/2ℤ)""" + System.lineSeparator() +
+                """––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––""" + System.lineSeparator() +
+                """0 (ℤ/2ℤ)     |  0 (ℤ/2ℤ)      1 (ℤ/2ℤ)      X (ℤ/2ℤ)      X + 1 (ℤ/2ℤ)""" + System.lineSeparator() +
+                """1 (ℤ/2ℤ)     |  1 (ℤ/2ℤ)      0 (ℤ/2ℤ)      X + 1 (ℤ/2ℤ)  X (ℤ/2ℤ)    """ + System.lineSeparator() +
+                """X (ℤ/2ℤ)     |  X (ℤ/2ℤ)      X + 1 (ℤ/2ℤ)  0 (ℤ/2ℤ)      1 (ℤ/2ℤ)    """ + System.lineSeparator() +
+                """X + 1 (ℤ/2ℤ) |  X + 1 (ℤ/2ℤ)  X (ℤ/2ℤ)      1 (ℤ/2ℤ)      0 (ℤ/2ℤ)    """
         val actual = table.format(2, OperationTable.FormatStyle.PRETTY)
         println(actual)
         assertEquals(expected, actual)
